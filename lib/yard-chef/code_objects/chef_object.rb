17c6b7238a90f849d7ca976f0c74d710666dd93c
Steven Wagner <steven.wagner@disney.com>
2013-06-21 11:08:03 -0700

    Added docstring coverage stats for Chef objects.

--- lib/yard-chef/code_objects/chef_object.rb
+++ lib/yard-chef/code_objects/chef_object.rb
@@ -106,3 +106,41 @@ module YARD::CodeObjects
     log.info "Creating [Chef] as 'root' namespace"
   end
 end
+
+class YARD::CLI::Stats
+  def stats_for_recipes
+    objs = all_objects.select {|m| m.type == :recipe }
+    #objs.reject! {|m| m.is_alias? || !m.is_explicit? }
+    undoc = objs.select {|m| m.docstring.blank? }
+    @undoc_list |= undoc if @undoc_list
+    output "Recipes", objs.size, undoc.size
+  end
+  def stats_for_resources
+    objs = all_objects.select {|m| m.type == :resource }
+    #objs.reject! {|m| m.is_alias? || !m.is_explicit? }
+    undoc = objs.select {|m| m.docstring.blank? }
+    @undoc_list |= undoc if @undoc_list
+    output "Resources", objs.size, undoc.size
+  end
+  def stats_for_defines
+    objs = all_objects.select {|m| m.type == :define }
+    #objs.reject! {|m| m.is_alias? || !m.is_explicit? }
+    undoc = objs.select {|m| m.docstring.blank? }
+    @undoc_list |= undoc if @undoc_list
+    output "Defines", objs.size, undoc.size
+  end
+  def stats_for_actions
+    objs = all_objects.select {|m| m.type == :action }
+    #objs.reject! {|m| m.is_alias? || !m.is_explicit? }
+    undoc = objs.select {|m| m.docstring.blank? }
+    @undoc_list |= undoc if @undoc_list
+    output "Actions", objs.size, undoc.size
+  end
+  def stats_for_attributes
+    objs = all_objects.select {|m| m.type == :attribute }
+    #objs.reject! {|m| m.is_alias? || !m.is_explicit? }
+    undoc = objs.select {|m| m.docstring.blank? }
+    @undoc_list |= undoc if @undoc_list
+    output "Attributes", objs.size, undoc.size
+  end
+end
